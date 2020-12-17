734ca83bba4853ae384937624f183dbf5cdeebac
Max Kaliakin <max.kaliakin@gmail.com>
2020-12-17 14:24:54 +0200

    add anim for sub-headers and update nav buttons on index page

--- app/js/mainPageAnimation.js
+++ app/js/mainPageAnimation.js
@@ -1,6 +1,38 @@
+// UPDATE VERTICAL NAV BUTTON ON LOAD
+let activeSwipeIndex = 0;
+
+const downButton = document.getElementById("next-button");
+const upButton = document.getElementById("prev-button");
+
+const show = (el) => {
+  el.style.display = "flex";
+};
+const hide = (el) => {
+  el.style.display = "none";
+};
+
+const handleButtonVisibility = () => {
+  const firstCond = activeSwipeIndex === 0;
+  const secondCond = activeSwipeIndex === slidersCollection.length - 1;
+
+  if (firstCond) {
+    show(downButton);
+    hide(upButton);
+  }
+  if (secondCond) {
+    show(upButton);
+    hide(downButton);
+  } else if (!firstCond && !secondCond) {
+    show(upButton);
+    show(downButton);
+  }
+};
+
+document.addEventListener("DOMContentLoaded", handleButtonVisibility);
+
 // // PREPARING ACTIVE SLIDER
 const slidersCollection = document.getElementsByClassName("swiper-slide");
-let activeSwipeIndex = 0;
+
 const activeItem = `s_item_${activeSwipeIndex}`;
 
 // SWIPER INIT + SET OPTIONS ON INITIAL START
@@ -49,10 +81,18 @@ const swiper = new Swiper(".mainPageSwiper", {
 
 const defaultTextAnimation = () => {
   anime({
-    targets: `.s_item_${activeSwipeIndex} .s_item--text`,
+    targets: `.s_item_${activeSwipeIndex} .s_item--text .h1`,
     translateX: [-50, 0],
     opacity: [0, 1],
     delay: 0,
+    easing: "easeInOutQuad",
+    duration: 2000,
+  });
+  anime({
+    targets: `.s_item_${activeSwipeIndex} .s_item--text .index_sub_header`,
+    translateX: [-50, 0],
+    opacity: [0, 1],
+    delay: 500,
     easing: "easeInOutQuad",
     duration: 2000,
   });
@@ -84,24 +124,24 @@ const handIosImgBlock = (activeSwipeIndex) => {
 
 const handPlaneImgBlock = (activeSwipeIndex) => {
   defaultTextAnimation();
-  anime({
-    targets: `.s_item_${activeSwipeIndex} .s_item--images .plane-dot`,
-    translateX: [400, 0],
-    opacity: [0, 1],
+  // anime({
+  //   targets: `.s_item_${activeSwipeIndex} .s_item--images .plane-dot`,
+  //   translateX: [400, 0],
+  //   opacity: [0, 1],
 
-    delay: 1000,
-    easing: "easeInOutQuad",
-    duration: 2000,
-  });
+  //   delay: 1000,
+  //   easing: "easeInOutQuad",
+  //   duration: 2000,
+  // });
   anime({
     targets: `.s_item_${activeSwipeIndex} .plane`,
     opacity: [0, 1],
     scale: [0.5, 1],
     translateX: [-200, 0],
     translateY: [200, 0],
-    delay: 2000,
+    delay: 1000,
     easing: "easeInOutQuad",
-    duration: 4000,
+    duration: 2000,
   });
 };
 
@@ -119,6 +159,7 @@ const girlImgBlock = (activeSwipeIndex) => {
 // ON SLIDER CHANGE
 swiper.on("slideChange", function () {
   activeSwipeIndex = swiper.activeIndex;
+  handleButtonVisibility();
   switch (activeSwipeIndex) {
     case 0:
       ringPhoneBlock(activeSwipeIndex);
